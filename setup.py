096b1529830e3b4a4011f61717561d3c31b0929f
Oisin McNally <oisin.mcnally@genesys.com>
2020-09-21 15:48:39 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ForexTradingBot',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='ushmau5',
+    author_email='oisin097@hotmail.com',
+    description=''
+)
